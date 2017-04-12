3f269c43bf2725cb92faeeffd1a1cc855b0bc759
Mauriyin <mauri@foxmail.com>
2017-04-12 22:54:06 +0800

    finish math

--- Math/Math2D.c
+++ Math/Math2D.c
@@ -6,6 +6,9 @@
 */
 int StaticPointToStaticCircle(Vector2D *pP, Vector2D *pCenter, float Radius)
 {
+	float dis = Vector2DDistance(pP, pCenter);
+	if (dis <= Radius)
+		return 1;
 	return 0;
 }
 
@@ -15,7 +18,11 @@ int StaticPointToStaticCircle(Vector2D *pP, Vector2D *pCenter, float Radius)
 */
 int StaticPointToStaticRect(Vector2D *pPos, Vector2D *pRect, float Width, float Height)
 {
-  return 0;
+	float x = fabs(pPos->x - (pRect->x));
+	float y = fabs(pPos->y - (pRect->y));
+	if (x <= Width / 2 && y <= Height / 2)
+		return 1;
+	return 0;
 }
 
 /*
@@ -24,7 +31,10 @@ Circle1: 圆心pCenter1，半径pRadius1
 */
 int StaticCircleToStaticCircle(Vector2D *pCenter0, float Radius0, Vector2D *pCenter1, float Radius1)
 {
-  return 0;
+	float dis = Vector2DDistance(pCenter0, pCenter1);
+	if (dis <= Radius0 + Radius1)
+		return 1;
+	return 0;
 }
 
 /*
@@ -34,5 +44,9 @@ Rectangle1: 中心pRect1，宽度Width1，高度Height1
 */
 int StaticRectToStaticRect(Vector2D *pRect0, float Width0, float Height0, Vector2D *pRect1, float Width1, float Height1)
 {
-  return 0;
+	float x = fabs(pRect0->x - (pRect1->x));
+	float y = fabs(pRect0->y - (pRect1->y));
+	if (x <= fabs(Width0+Width1) / 2 && y <= fabs(Height0+Height1) / 2)
+		return 1;
+	return 0;
 }
